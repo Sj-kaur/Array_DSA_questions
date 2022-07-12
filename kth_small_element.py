c255c2b3bc6daf0f48494cbdbbd3beff925fe884
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2022-07-12 16:00:31 +0530

    Add files via upload

---
+++ kth_small_element.py
@@ -0,0 +1,18 @@
+def kth_smallest_element(arr,n,k):
+    if n > k:
+        return arr[k-1]
+
+n = int(input("Enter the length of array: \n"))
+k = int(input("Enter the smallest element number to find. It should be less than the number of elements. "))
+print("Enter the elements : ")
+arr =[]
+for i in range(1,n+1):
+    value = int(input())
+    arr.append(value)
+print("The array is : ",arr)
+arr.sort()
+print("Sorted array is :",arr)
+print(k ,"th smallest element is : ",kth_smallest_element(arr,n,k))
+
+
+       
