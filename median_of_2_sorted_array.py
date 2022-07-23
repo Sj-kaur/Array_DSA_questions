fdce13c91773897c8a94ffd092e48131e0d6ef71
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2022-07-23 12:50:19 +0530

    Add files via upload

---
+++ median_of_2_sorted_array.py
@@ -0,0 +1,28 @@
+
+
+def median_array(arr):
+    i = int(len(arr)/2)
+    avg = (arr[i] + arr[i-1])/2
+    return int(avg)
+
+
+if __name__ == "__main__":
+    n1 = int(input("Enter the length for first array: \n"))
+    print("Enter the elements : ")
+    arr1 = []
+    for i in range(n1):
+        value = int(input())
+        arr1.append(value)
+
+    n2 = int(input("Enter the length for second array: \n"))
+    print("Enter the elements : ")
+    arr2 = []
+    for i in range(n2):
+        value = int(input())
+        arr2.append(value)
+
+    arr1.sort()
+    arr2.sort()
+    arr = (arr1 + arr2)
+    arr.sort()
+    print("The median of two sorted array is :",median_array(arr))
