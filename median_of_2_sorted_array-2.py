f7661ab718da7102dc6ed195f903cc0e4df92fb9
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2022-07-24 12:45:38 +0530

    Update median_of_2_sorted_array-2.py

--- median_of_2_sorted_array-2.py
+++ median_of_2_sorted_array-2.py
@@ -2,7 +2,7 @@
 
 def median_array(arr):
     if len(arr)%2 == 0 :
-        return ((arr[(len(arr)-1)//2])+arr[(len(arr)-1)//2+1])//2
+        return ((arr[(len(arr)-1)//2])+arr[(len(arr)-1)//2+1])/2
     else :
         return arr[len(arr)//2]
 
