46c502cd46e8bf1aeefa9e1d75a8754c88dbef9d
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2022-07-26 16:42:10 +0530

    Add files via upload

---
+++ kadanes_algo.py
@@ -0,0 +1,22 @@
+
+def maxSubArraySum(arr,n):
+    max_far = arr[0]
+    max_end_here = 0
+
+    for i in range(n):
+        max_end_here = max_end_here + arr[i]
+        if max_end_here < 0:
+            max_end_here = 0
+        elif max_end_here > max_far:
+            max_far = max_end_here
+    return max_far
+
+if __name__ == "__main__":
+    n = int(input("Enter the length of array: \n"))
+    print("Enter the elements : ")
+    arr =[]
+    for i in range(n):
+        value = int(input())
+        arr.append(value)
+    print("The array is : ",arr)
+    print(maxSubArraySum(arr,n))
